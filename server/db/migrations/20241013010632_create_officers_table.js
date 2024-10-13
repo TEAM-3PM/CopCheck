07ef215091310c9a8f6c887aea73009867c572f3
Mekhi-M <mekhim0731@gmail.com>
2024-10-12 21:13:37 -0400

    Backend - Officers Table - Implementation

---
+++ server/db/migrations/20241013010632_create_officers_table.js
@@ -0,0 +1,26 @@
+/**
+ * @format
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.up = knex => {
+	return knex.schema.createTable("officers", table => {
+		table.increments("id").primary();
+		table.integer("tax_id").notNullable();
+		table.foreign("tax_id").references("id").inTable("public_data");
+		table.integer("precinct_id").notNullable();
+		table.foreign("precinct_id").references("id").inTable("precincts");
+		table.string("first_name").notNullable();
+		table.string("last_name").notNullable();
+		table.integer("badge_num").notNullable();
+		table.timestamps(true, true);
+		table.boolean("active_duty").notNullable();
+	});
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = knex => knex.schema.dropTable("officers");
