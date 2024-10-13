9d24fe6cdfd61729d5e647bf646f0ad19d5ad9cc
Mekhi-M <mekhim0731@gmail.com>
2024-10-12 21:16:18 -0400

    Backend - Precincts Table - Implementation

---
+++ server/db/migrations/20241013011421_create_precincts_table.js
@@ -0,0 +1,18 @@
+/**
+ * @format
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.up = knex => {
+	return knex.schema.createTable("precincts", table => {
+		table.increments("id").primary();
+		table.string("borough").notNullable();
+	});
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = knex => knex.schema.dropTable("precincts");
