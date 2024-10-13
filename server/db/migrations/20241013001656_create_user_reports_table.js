303eec9bb79dbc3943c210475a13b249f55f3240
Mekhi-M <mekhim0731@gmail.com>
2024-10-12 20:57:12 -0400

    Backend - User Posts Table - Bug Fix

--- server/db/migrations/20241013001656_create_user_reports_table.js
+++ server/db/migrations/20241013001656_create_user_reports_table.js
@@ -7,7 +7,9 @@
 exports.up = function (knex) {
 	return knex.schema.createTable("user_reports", table => {
 		table.increments("id").primary();
+		table.integer("user_id").notNullable();
 		table.foreign("user_id").references("id").inTable("users");
+		table.integer("officer_id").notNullable();
 		table.foreign("officer_id").references("id").inTable("officers");
 		table.timestamps(true, true);
 	});
