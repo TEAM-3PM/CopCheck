cea36aae584b1e479c7c312eaaebd8ef30cf668c
Mekhi-M <mekhim0731@gmail.com>
2024-10-13 17:59:34 -0400

    Backend - Log In - Testing
    Added another seed into the database. Login works with the seeds as expected

--- server/db/seeds/init.js
+++ server/db/seeds/init.js
@@ -1,22 +1,34 @@
+/** @format */
+
 const User = require("../../models/User");
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.seed = async (knex) => {
-  // Before you have models you can always just do `await knex('table_name').del`
-  await knex("users").del();
+exports.seed = async knex => {
+	// Before you have models you can always just do `await knex('table_name').del`
+	await knex("users").del();
 
-  await knex.raw("ALTER SEQUENCE users_id_seq RESTART WITH 1");
+	await knex.raw("ALTER SEQUENCE users_id_seq RESTART WITH 1");
 
-  // User.create(username, password)
-  await User.create(
-    "cool_cat",
-    "1234",
-    "email@gmail.com",
-    "mekhi tudor",
-    22,
-    "black",
-    "man"
-  );
+	// User.create(username, password, email, full_name, age, race, gender)
+	await User.create(
+		"cool_cat",
+		"1234",
+		"email@gmail.com",
+		"mekhi tudor",
+		22,
+		"black",
+		"man"
+	);
+
+	await User.create(
+		"stonky",
+		"sus",
+		"yesno@maybe.so",
+		"Mekhi Miller",
+		21,
+		"Black",
+		"Male"
+	);
 };
