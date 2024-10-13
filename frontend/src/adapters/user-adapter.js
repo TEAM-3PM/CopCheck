cb7c991a8ed191a00e9af28cee83e96e149fbff2
Mekhi-M <mekhim0731@gmail.com>
2024-10-13 18:13:51 -0400

    Fullstack - Sign Up - Testing
    Commented on files which will need to be changed to fix Sign Up

--- frontend/src/adapters/user-adapter.js
+++ frontend/src/adapters/user-adapter.js
@@ -1,26 +1,36 @@
+/** @format */
+
 // These functions all take in a body and return an options object
 // with the provided body and the remaining options
-import { fetchHandler, getPostOptions, getPatchOptions } from "../utils/fetchingUtils";
+import {
+	fetchHandler,
+	getPostOptions,
+	getPatchOptions,
+} from "../utils/fetchingUtils";
 
-const baseUrl = '/api/users';
+const baseUrl = "/api/users";
 
 export const createUser = async ({ username, password }) => {
-  return fetchHandler(baseUrl, getPostOptions({ username, password }))
+	return fetchHandler(baseUrl, getPostOptions({ username, password }));
+	// error goes through here
+	/** Fix:
+	 * Add new required parameters to the functions
+	 * Pass the parameters into the getPostOptions() function
+	 */
 };
 
 // For this one adapter, if an error occurs, we handle it here by printing
 // the error and return an empty array
 export const getAllUsers = async () => {
-  const [users, error] = await fetchHandler(baseUrl);
-  if (error) console.log(error); // print the error for simplicity.
-  return users || [];
+	const [users, error] = await fetchHandler(baseUrl);
+	if (error) console.log(error); // print the error for simplicity.
+	return users || [];
 };
 
-export const getUser = async (id) => {
-  return fetchHandler(`${baseUrl}/${id}`);
-}
+export const getUser = async id => {
+	return fetchHandler(`${baseUrl}/${id}`);
+};
 
 export const updateUsername = async ({ id, username }) => {
-  return fetchHandler(`${baseUrl}/${id}`, getPatchOptions({ id, username }))
-}
-
+	return fetchHandler(`${baseUrl}/${id}`, getPatchOptions({ id, username }));
+};
--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,40 +1,47 @@
-const { isAuthorized } = require('../utils/auth-utils');
-const User = require('../models/User');
+/** @format */
+
+const { isAuthorized } = require("../utils/auth-utils");
+const User = require("../models/User");
 
 exports.createUser = async (req, res) => {
-  const { username, password } = req.body;
+	const { username, password } = req.body;
 
-  // TODO: check if username is taken, and if it is what should you return?
-  const user = await User.create(username, password);
-  req.session.userId = user.id;
+	// TODO: check if username is taken, and if it is what should you return?
+	const user = await User.create(username, password);
+	// error goes through here
+	/** Fix:
+	 * Deconstruct the needed values from the fetch request's body
+	 * Pass those values into User.create() in the correct order
+	 */
+	req.session.userId = user.id;
 
-  res.send(user);
+	res.send(user);
 };
 
 exports.listUsers = async (req, res) => {
-  const users = await User.list();
-  res.send(users);
+	const users = await User.list();
+	res.send(users);
 };
 
 exports.showUser = async (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  const user = await User.find(id);
-  if (!user) return res.sendStatus(404);
+	const user = await User.find(id);
+	if (!user) return res.sendStatus(404);
 
-  res.send(user);
+	res.send(user);
 };
 
 exports.updateUser = async (req, res) => {
-  const { username } = req.body;
-  const { id } = req.params;
+	const { username } = req.body;
+	const { id } = req.params;
 
-  // Not only do users need to be logged in to update a user, they
-  // need to be authorized to perform this action for this particular
-  // user (users should only be able to change their own profiles)
-  if (!isAuthorized(id, req.session)) return res.sendStatus(403);
+	// Not only do users need to be logged in to update a user, they
+	// need to be authorized to perform this action for this particular
+	// user (users should only be able to change their own profiles)
+	if (!isAuthorized(id, req.session)) return res.sendStatus(403);
 
-  const updatedUser = await User.update(id, username);
-  if (!updatedUser) return res.sendStatus(404)
-  res.send(updatedUser);
+	const updatedUser = await User.update(id, username);
+	if (!updatedUser) return res.sendStatus(404);
+	res.send(updatedUser);
 };
