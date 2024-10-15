95c6e8cfbd128fdbe3a42716fe095d7d239bf4fa
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-10-15 14:25:49 -0400

    forgot the add

---
+++ server/models/Content.js
@@ -0,0 +1,62 @@
+const knex = require("../db/knex");
+const authUtils = require("../utils/auth-utils");
+
+class Content {
+  // Fetches ALL users from the users table, uses the constructor
+  // to format each user (and hide their password hash), and returns.
+  static async list() {
+    const query = `SELECT * FROM contents`;
+    const result = await knex.raw(query);
+    return result.rows.map((rawUserData) => new Content(rawUserData));
+  }
+
+  // Fetches A single user from the users table that matches
+  // the given user id. If it finds a user, uses the constructor
+  // to format the user and returns or returns null if not.
+  static async find(id) {
+    const query = `SELECT * FROM contents WHERE id = ?`;
+    const result = await knex.raw(query, [id]);
+    const rawUserData = result.rows[0];
+    return rawUserData ? new Content(rawUserData) : null;
+  }
+
+  // Same as above but uses the username to find the user
+  static async findByUsername(username) {
+    const query = `SELECT * FROM contents WHERE username = ?`;
+    const result = await knex.raw(query, [username]);
+    const rawUserData = result.rows[0];
+    return rawUserData ? new Content(rawUserData) : null;
+  }
+
+  // Hashes the given password and then creates a new user
+  // in the users table. Returns the newly created user, using
+  // the constructor to hide the passwordHash.
+  static async create(report_id, content, type) {
+    const query = `INSERT INTO contents (report_id, content, type)
+      VALUES (?, ?, ?) RETURNING *`;
+    const result = await knex.raw(query, [report_id, content, type]);
+    const rawUserData = result.rows[0];
+    return new Content(rawUserData);
+  }
+
+  // Updates the user that matches the given id with a new username.
+  // Returns the modified user, using the constructor to hide the passwordHash.
+
+  static async updateContent(id, content) {
+    const query = `
+      UPDATE contents
+      SET content=?
+      WHERE id=?
+      RETURNING *
+    `;
+    const result = await knex.raw(query, [full_name, id]);
+    const rawUpdatedUser = result.rows[0];
+    return rawUpdatedUser ? new Content(rawUpdatedUser) : null;
+  }
+
+  static async deleteAll() {
+    return knex("contents").del();
+  }
+}
+
+module.exports = Content;
---
+++ server/models/PublicComplaints.js
@@ -0,0 +1,93 @@
+//copy user model
+//link controller up
+const knex = require("../db/knex");
+
+class PublicComplaints {
+  static async list() {
+    const query = `SELECT * FROM public_complaints`;
+    const result = await knex.raw(query);
+    return result.rows.map((rawUserData) => new PublicComplaints(rawUserData));
+  }
+
+  static async find(id) {
+    const query = `SELECT * FROM public_complaints WHERE id = ?`;
+    const result = await knex.raw(query, [id]);
+    const rawPublicData = result.rows[0];
+    return rawPublicData ? new PublicComplaints(rawPublicData) : null;
+  }
+
+  static async create(
+    id,
+    as_of_date,
+    complaint_id,
+    complaint_officer_number,
+    tax_id,
+    officer_rank_abbreviation_at_incident,
+    officer_rank_at_incident,
+    officer_command_at_incident,
+    officer_days_on_force_at_incident,
+    allegation_record_identity,
+    fado_type,
+    allegation,
+    victim_allegedvictim_age_range_at_incident,
+    victim_allegedvictim_gender,
+    victim_allegedvictim_race_legacy,
+    victim_alleged_victim_race_ethnicity,
+    ccrb_allegation_disposition,
+    nypd_allegation_disposition,
+    investigator_recommended_allegation_disposition
+  ) {
+    const query = `INSERT INTO public_complaints ( id,
+    as_of_date,
+    complaint_id,
+    complaint_officer_number,
+    tax_id,
+    officer_rank_abbreviation_at_incident,
+    officer_rank_at_incident,
+    officer_command_at_incident,
+    officer_days_on_force_at_incident,
+    allegation_record_identity,
+    fado_type,
+    allegation,
+    victim_allegedvictim_age_range_at_incident,
+    victim_allegedvictim_gender,
+    victim_allegedvictim_race_legacy,
+    victim_alleged_victim_race_ethnicity,
+    ccrb_allegation_disposition,
+    nypd_allegation_disposition,
+    investigator_recommended_allegation_disposition )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) RETURNING *`;
+    const result = await knex.raw(query, [
+      id,
+      as_of_date,
+      complaint_id,
+      complaint_officer_number,
+      tax_id,
+      officer_rank_abbreviation_at_incident,
+      officer_rank_at_incident,
+      officer_command_at_incident,
+      officer_days_on_force_at_incident,
+      allegation_record_identity,
+      fado_type,
+      allegation,
+      victim_allegedvictim_age_range_at_incident,
+      victim_allegedvictim_gender,
+      victim_allegedvictim_race_legacy,
+      victim_alleged_victim_race_ethnicity,
+      ccrb_allegation_disposition,
+      nypd_allegation_disposition,
+      investigator_recommended_allegation_disposition,
+    ]);
+    const rawUserData = result.rows[0];
+    return new PublicComplaints(rawUserData);
+  }
+
+  // Updates the user that matches the given id with a new username.
+  // Returns the modified user, using the constructor to hide the passwordHash.
+
+  static async deleteAll() {
+    return knex("public_reports").del();
+  }
+}
+
+module.exports = PublicReports;
