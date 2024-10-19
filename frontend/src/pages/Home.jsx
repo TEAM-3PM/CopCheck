c224ccff7d5cb0a04c8f93fbffff63a68b6a30c6
Patrick Dacius <Patrickdacius27@gmail.comgit config --global credential.helper store>
2024-10-19 19:39:45 -0400

    users are able to see their reports on the HomePage along with all of the content (text, image, videos), and officers that are associated with the report

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,10 @@
+import React, { useEffect } from "react";
+import ListOfReports from "./ListOfReports";
+
+
 export default function HomePage() {
   return (
-    <>
+    <div>
       <h1>
         Put
         <br />
@@ -9,6 +13,7 @@ export default function HomePage() {
         Back in Your Hands...
       </h1>
       <p>Check the Authority!</p>
-    </>
+      <ListOfReports />
+    </div>
   );
 }
