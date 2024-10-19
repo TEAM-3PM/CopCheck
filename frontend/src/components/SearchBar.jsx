1bcd73586c8ecb2de36f93e9b4a2d946a495b30d
Mekhi-M <mekhim0731@gmail.com>
2024-10-19 18:28:28 -0400

    Frontend - Search Bar - Elements Complete

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -16,19 +16,32 @@ export const SearchBar = ({ optionStart }) => {
 		} catch (error) {}
 	};
 
+	const handleChange = e => {
+		const { name, value } = e.target;
+		if (name === "query") setQuery(value);
+		if (name === "searchOptions") setSearchOption(value);
+	};
+
 	return (
 		<form
 			aria-label='Search Bar'
 			onSubmit={handleSubmit}>
 			<input
 				type='text'
+				name='query'
 				value={query}
+				onChange={handleChange}
 				required
 			/>
 			<select
 				name='searchOptions'
-				value={searchOption}></select>
-			<button type='submit'></button>
+				value={searchOption}
+				onChange={handleChange}>
+				<option value='name'>Last Name</option>
+				<option value='badge'>Badge No.</option>
+				<option value='precinct'>Precinct</option>
+			</select>
+			<button type='submit'>Search</button>
 		</form>
 	);
 };
