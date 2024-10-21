cf8c1a6e9a90daf7d8686626b3dfc89001beba3d
Mekhi-M <mekhim0731@gmail.com>
2024-10-21 13:14:20 -0400

    Backend - Officer Controllers - Last Name Search

--- server/controllers/officerControllers.js
+++ server/controllers/officerControllers.js
@@ -3,6 +3,24 @@
 const Officer = require("../models/Officer");
 
 exports.listOfficers = async (req, res) => {
-  const officers = await Officer.list();
-  res.send(officers);
+	const officers = await Officer.list();
+	res.send(officers);
+};
+
+exports.resultsOfficerByLastName = async (req, res) => {
+	const { last_name } = this.requestData(req);
+
+	try {
+		const searchResults = await Officer.findByLastName(last_name);
+
+		res.status(200).send(searchResults);
+	} catch (error) {
+		res.status(500).send("Error retrieving officers");
+	}
+};
+
+exports.requestData = req => {
+	const { last_name } = req.params;
+
+	return { last_name };
 };
