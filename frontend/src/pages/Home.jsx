674becebfc462fb8590099e22ab66aa6f31725b6
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-10-22 00:02:20 -0400

    Linked up search endpoints with form

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,14 @@
 import React, { useEffect } from "react";
 import ListOfReports from "./ListOfReports";
-
+import Slogan from "../Cop Check Assets/2.png";
 
 export default function HomePage() {
   return (
     <div>
-      <h1>
-        Put
-        <br />
-        The Power
-        <br />
-        Back in Your Hands...
-      </h1>
-      <p>Check the Authority!</p>
+      <section id="slogan-section">
+        <img src={Slogan} alt="" id="HomePageSlogan" />
+      </section>
+
       <ListOfReports />
     </div>
   );
