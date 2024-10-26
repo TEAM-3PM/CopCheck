f7a32fc720e48d03231a57893a0889608612f2bd
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-10-26 13:46:42 -0400

    update Comment

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -3,9 +3,16 @@ const Comments = require("../models/Comments");
 //create comment
 exports.CreateComments = async (req, res) => {
   const { user_id, report_id, text } = req.body;
-  const comment = await Comments(user_id, report_id, text);
+  const comment = await Comments.create(user_id, report_id, text);
   res.send(comment);
 };
 //update
+exports.UpdateComments = async (req, res) => {
+  const { text } = req.body;
+  const { id } = req.params;
+  const updatedComment = await Comments.updateComment(id, text);
+  if (!updatedComment) return res.sendStatus(404);
+  res.send(comment);
+};
 
 //delete comment
