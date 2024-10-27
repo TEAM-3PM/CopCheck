95700034feeb637de7dd4d5848b475b01e5150d8
Mekhi-M <mekhim0731@gmail.com>
2024-10-27 15:12:03 -0400

    Backend - Content Seeding - Updated

--- server/db/seeds/05_contents.js
+++ server/db/seeds/05_contents.js
@@ -9,14 +9,19 @@ const Content = require("../../models/Content");
  */
 
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
-  await knex("contents").del();
+	// Deletes ALL existing entries
+	await knex("contents").del();
 
-  await knex.raw("ALTER SEQUENCE contents_id_seq RESTART WITH 1");
+	await knex.raw("ALTER SEQUENCE contents_id_seq RESTART WITH 1");
 
-  // create(officer_id, content, type, user_id);
-  await Content.create(1, "i hate this cop", "text", 1);
-  await Content.create(4, "i hate this cop", "text", 2);
-  await Content.create(4, "this cop sucks too", "text", 4);
-  await Content.create(3, "this cop strip searched me", "text", 4);
+	// create(report_id, content, type, user_id);
+	// every content should have ONE text content
+	// you must correctly link both the report id and the user id
+	await Content.create(1, "i hate this cop", "text", 1);
+
+	await Content.create(2, "i hate this cop", "text", 1);
+
+	await Content.create(3, "this cop sucks too", "text", 2);
+
+	await Content.create(4, "this cop strip searched me", "text", 3);
 };
