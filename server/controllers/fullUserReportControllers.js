fb790721ccd3179920f0edec3a776fb342632638
Mekhi-M <mekhim0731@gmail.com>
2024-10-27 13:28:00 -0400

    Fullstack - Report POST - Bug Fix
    implemented more bug solving console.logs

--- server/controllers/fullUserReportControllers.js
+++ server/controllers/fullUserReportControllers.js
@@ -6,70 +6,73 @@ const FullUserReport = require("../models/FullUserReport");
 const Comments = require("../models/Comments");
 
 exports.createFullUserReport = async (req, res) => {
-  const { currUserId, body_officer_id, contents } = this.requestData(req);
+	const { currUserId, body_officer_id, contents } = this.requestData(req);
 
-  try {
-    const userReport = await UserReport.create(currUserId, body_officer_id);
+	try {
+		const userReport = await UserReport.create(currUserId, body_officer_id);
 
-    const reportContents = await Promise.all(
-      contents.map(({ content, type }) =>
-        Content.create(userReport.id, content, type)
-      )
-    );
+		const reportContents = await Promise.all(
+			contents.map(({ content, type }) =>
+				Content.create(userReport.id, content, type, currUserId)
+			)
+		);
 
-    res.status(201).send(reportContents);
-  } catch (error) {
-    return res.status(500).send("Error creating report");
-  }
+		res.status(201).send(reportContents);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send("Error creating report");
+	}
 };
 
 exports.listFullUserReports = async (req, res) => {
-  try {
-    const userReports = await FullUserReport.list();
+	try {
+		const userReports = await FullUserReport.list();
 
-    res.status(200).send(userReports);
-  } catch (error) {
-    return res.status(500).send("Error retrieving report data");
-  }
+		res.status(200).send(userReports);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send("Error retrieving report data");
+	}
 };
 
 exports.listFullUserReportsForOfficer = async (req, res) => {
-  const { param_officer_id } = this.requestData(req);
+	const { param_officer_id } = this.requestData(req);
 
-  try {
-    const userReportsByOfficerId = await FullUserReport.findByOfficerId(
-      param_officer_id
-    );
+	try {
+		const userReportsByOfficerId = await FullUserReport.findByOfficerId(
+			param_officer_id
+		);
 
-    res.status(200).send(userReportsByOfficerId);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("Error retrieving report data");
-  }
+		res.status(200).send(userReportsByOfficerId);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send("Error retrieving report data");
+	}
 };
 
 exports.listFullUserReportsForUser = async (req, res) => {
-  const { user_id } = this.requestData(req);
+	const { user_id } = this.requestData(req);
 
-  try {
-    const userReportsByUserId = await FullUserReport.findByUserId(user_id);
-   
-    res.status(200).send(userReportsByUserId);
-  } catch (error) {
-    return res.status(500).send("Error retrieving report data");
-  }
+	try {
+		const userReportsByUserId = await FullUserReport.findByUserId(user_id);
+
+		res.status(200).send(userReportsByUserId);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send("Error retrieving report data");
+	}
 };
 
-exports.requestData = (req) => {
-  const { userId } = req.session;
-  const { officer_id: param_officer_id, user_id } = req.params;
-  const { officer_id: body_officer_id, contents } = req.body;
+exports.requestData = req => {
+	const { userId } = req.session;
+	const { officer_id: param_officer_id, user_id } = req.params;
+	const { officer_id: body_officer_id, contents } = req.body;
 
-  return {
-    currUserId: userId,
-    body_officer_id,
-    contents,
-    param_officer_id,
-    user_id,
-  };
+	return {
+		currUserId: userId,
+		body_officer_id,
+		contents,
+		param_officer_id,
+		user_id,
+	};
 };
