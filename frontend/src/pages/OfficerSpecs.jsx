9ef32525ed8dae429d0adeb5ad78f32f9cc4db7b
Mekhi-M <mekhim0731@gmail.com>
2024-10-27 15:10:11 -0400

    Frontend - Officer Specs - Updated
    omg im fing crying rn this was painful react logic SUCKS

--- frontend/src/pages/OfficerSpecs.jsx
+++ frontend/src/pages/OfficerSpecs.jsx
@@ -1,4 +1,4 @@
-
+/** @format */
 
 import React, { useState, useEffect } from "react";
 import { fetchHandler } from "../utils/fetchingUtils";
@@ -7,65 +7,43 @@ import { PublicComplaintList } from "../components/PublicComplaintList";
 import { UserReportList } from "../components/UserReportList";
 
 const OfficerSpecs = () => {
-    const { officerId } = useParams();
-    const [officer, setOfficer] = useState(null);
-    const [reports, setReports] = useState([]);
+	const { officerId } = useParams();
+	const [officer, setOfficer] = useState({});
+	const [reports, setReports] = useState([]);
 
-    useEffect(() => {
-        const fetchOfficer = async () => {
-            try {
-                const [data, error] = await fetchHandler(
-                    `/api/officers/${officerId}/complaints`
-                );
-                if (data) {
-                    setOfficer(data);
-                } else {
-                    console.error("Error fetching officer:", error);
-                }
-            } catch (error) {
-                console.error("Error fetching officer:", error);
-            }
-        };
+	useEffect(() => {
+		const fetchOfficer = async () => {
+			try {
+				const [data, error] = await fetchHandler(
+					`/api/officers/${officerId}/complaints/reports`
+				);
+				if (data && data?.reports) {
+					setOfficer(data);
+					setReports(data.reports);
+					// console.log(data);
+				}
+			} catch (error) {
+				console.error("Error fetching officer:", error);
+			}
+		};
+		fetchOfficer();
+	}, [officerId]);
 
-        const fetchPublicComplaints = async () => {
-            try {
-            } catch (error) { }
-        };
+	return (
+		<div className='col-flex'>
+			<h1>
+				Officer: {officer?.first_name} {officer?.last_name}
+			</h1>
+			<p>Badge Number: {officer?.badge_num}</p>
+			<div
+				className='row-flex fill-width'
+				style={{ justifyContent: "space-between", alignItems: "start" }}>
+				<PublicComplaintList officer={officer} />
 
-        const fetchReports = async () => {
-            try {
-                const [data, error] = await fetchHandler(
-                    `/api/reports/officer/${officerId}`
-                );
-                if (data) {
-                    setReports(data);
-                } else {
-                    console.error("Error fetching reports:", error);
-                }
-            } catch (error) {
-                console.error("Error fetching reports:", error);
-            }
-        };
-        fetchPublicComplaints();
-        fetchOfficer();
-        fetchReports();
-    }, [officerId]);
-
-    return (
-        <div className='col-flex'>
-            <h1>
-                Officer: {officer?.first_name} {officer?.last_name}
-            </h1>
-            <p>Badge Number: {officer?.badge_num}</p>
-            <div
-                className='row-flex fill-width'
-                style={{ justifyContent: "space-between", alignItems: "start" }}>
-                <PublicComplaintList officer={officer} />
-
-                <UserReportList reports={reports} />
-            </div>
-        </div>
-    );
+				<UserReportList reports={reports} />
+			</div>
+		</div>
+	);
 };
 
 export default OfficerSpecs;
