2d8920db9a8011c5a13fee7ed1efe23425aaf53f
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-10-27 22:17:54 -0400

    data comp

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import ListOfReports from "./ListOfReports";
 import Slogan from "../Cop Check Assets/2.png";
+import { DataComponent } from "../components/DataComponent";
 
 export default function HomePage() {
   return (
@@ -18,8 +19,10 @@ export default function HomePage() {
         </h1>
         <h4 className="authority-check">CHECK THE AUTHORITY!</h4>
       </div>
+
+      <div>
+        <DataComponent />
+      </div>
     </div>
   );
 }
-
-
