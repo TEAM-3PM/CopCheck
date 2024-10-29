7d4added0f29095cdf07b618719de0605c35dc96
Mekhi-M <mekhim0731@gmail.com>
2024-10-29 16:56:16 -0400

    putting auth back in

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.post("/api/users", userControllers.createUser);
 // These actions require users to be logged in (authentication)
 // Express lets us pass a piece of middleware to run for a specific endpoint
 app.get("/api/users", checkAuthentication, userControllers.listUsers);
-app.get("/api/users/:id", checkAuthentication, userControllers.showUser);
+app.get("/api/users/:id", checkAuthentication, userControllers.showUser); // we need check auth here
 app.patch("/api/users/:id", checkAuthentication, userControllers.updateUser);
 
 ///////////////////////////////
