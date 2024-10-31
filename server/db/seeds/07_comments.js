7031748b60511ce7e24f36ea2eca434c8e35c9b5
Patrick Dacius <patrickdacius27@gmail.com>
2024-10-31 11:02:46 -0400

    small changes for demo video

--- server/db/seeds/07_comments.js
+++ server/db/seeds/07_comments.js
@@ -10,6 +10,6 @@ exports.seed = async function (knex) {
   await knex("comments").del();
   await knex.raw("ALTER SEQUENCE comments_id_seq RESTART WITH 1");
 
-  await Comments.create(2, 1, "I'm sorry you had to go through this. Were there any witnesses or any further interactions after they pulled you out?");
-  await Comments.create(1, 2, "That sounds terrifying. Have you considered filing a formal complaint or seeking legal advice? It’s important they’re held accountable for this kind of behavior.");
+  await Comments.create(1, 2, "I'm sorry you had to go through this. Were there any witnesses or any further interactions after they pulled you out?");
+  await Comments.create(2, 1, "That sounds terrifying. Have you considered filing a formal complaint or seeking legal advice? It’s important they’re held accountable for this kind of behavior.");
 };
